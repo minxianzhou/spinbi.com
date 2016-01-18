ebcf99baa49590077cc8737ee283cfdbb71eb821
minxianzhou <minxianzhou@gmail.com>
2016-01-18 15:10:06 -0500

    init project

---
+++ material/client/app/app.module.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Core modules
+         'app.core'
+        
+        // Custom Feature modules
+        ,'app.chart'
+        ,'app.ui'
+        ,'app.ui.form'
+        ,'app.ui.form.validation'
+        ,'app.page'
+        ,'app.table'
+        
+        // 3rd party feature modules
+        ,'ui.tree'
+        ,'ngMap'
+        ,'textAngular'
+    ]);
+
+})();
+
