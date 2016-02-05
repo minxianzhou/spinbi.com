9d154f7eaad76747d239765c4200e14213f7fcc7
minxianzhou <minxianzhou@gmail.com>
2016-02-05 17:09:38 -0500

    update

--- material/client/app/services/services.js
+++ material/client/app/services/services.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module(
+var app_service = angular.module(
 	'app.services',
 	[
 		'app.services.translation',
@@ -9,3 +9,45 @@ angular.module(
 		'app.services.dialog'
 	]
 );
+
+
+
+
+app_service.factory('authInterceptor', function ($rootScope, $q, $window) {
+  return {
+    request: function (config) {
+	    config.headers = config.headers || {};
+
+	    config.headers['Authorization'] =  'test andy';
+
+
+  		if ($window.localStorage.token) {
+        	config.headers['Authorization'] =  $window.localStorage.token;
+  		}
+  		return config;
+    },
+    responseError: function (response) {
+      console.log(response.status);
+      if (response.status === 401) {
+        // handle the case where the user is not authenticated
+      }
+      if (response.status === 403) {
+
+        var pathname = $window.location.pathname.substr($window.location.pathname.indexOf("/")+1).split(/\//)[0];
+        if(pathname == 'admin'){
+          window.location = '/admin/login';
+        }
+        else window.location = '/agent/login';
+      }
+      return response || $q.when(response);
+    }
+  };
+})
+
+
+.config(function ($httpProvider) {
+  $httpProvider.defaults.headers.post['XSRF-AUTH'] = 
+        "some accessToken to be generated later";
+  $httpProvider.interceptors.push('authInterceptor');
+});
+
